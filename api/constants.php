c6638412823653b66b4c4fe0a63a1124fe4bb5c3
kirgudu1 <sitezabota@gmail.com>
2025-05-30 18:24:48 +0300

    Add files via upload

--- api/constants.php
+++ api/constants.php
@@ -2,4 +2,4 @@
 // constants.php
 
 define('SITE_NAME', 'Рейтинг Купальников'); // или курс по маникюру, если речь о другом проекте
-define('SITE_URL', 'https://kursi-shitia-111.vercel.app/');
+define('SITE_URL', 'https://kursi-kosmetologa.vercel.app/');
